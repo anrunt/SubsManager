d9dad63dd08dddb8b9c2702dec9eb39c2a5446f0
anrunt <grunt.janek@gmail.com>
2025-05-22 22:42:04 +0200

    Okay i didnt stage xd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,14 @@
-<h1>Welcome</h1>
+<script lang="ts">
+// Two urls for different logins 
+// 1. For getting subscriptions json 
+// 2. For uploading json and adding subs to new account
+</script>
+
+<div class="min-h-screen flex flex-col items-center justify-center gap-4">
+  <a href="googleAuth" class="w-72 h-16 text-xl rounded-md flex cursor-pointer items-center justify-center bg-black p-2 text-white">
+    Download Subscriptions
+  </a>
+  <a href="uploadSubs" class="w-72 h-16 text-xl rounded-md flex cursor-pointer items-center justify-center bg-black p-2 text-white">
+    Import Subscriptions
+  </a>
+</div>
---
+++ src/routes/googleAuth/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  const url = "/";
+</script>
+
+<div class="flex h-screen w-full items-center justify-center">
+  <a href={url} class="flex items-center jusitfy-center">Google Login</a>
+</div>
