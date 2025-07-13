5f0214a027bc187b6665f65ddbd9db458f55a37e
anrunt <grunt.janek@gmail.com>
2025-07-13 16:23:19 +0200

    Navbar now shows login and logout buttons

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import "../../app.css";
+  import { enhance } from "$app/forms";
+  import { LogIn, LogOut } from "lucide-svelte";
 
   let { children, data } = $props();
 </script>
@@ -7,17 +9,30 @@
 <nav class="bg-black border-b border-gray-900 p-4">
   <div class="mx-0.5 flex items-center justify-between">
     <div class="flex items-center">
-      <a href="/" class="text-white text-2xl font-bold hover:text-gray-300 transition-colors">
+      <div class="text-white text-2xl font-bold hover:text-gray-300 transition-colors cursor-pointer">
         SubsManager
-      </a>
+      </div>
     </div>
 
-    <a
-      href="/login"
-      class="flex items-center justify-center text-md w-32 h-12 bg-white hover:bg-gray-300 text-black px-4 py-2 rounded-lg transition-colors"
-    >
-      Login
-    </a>
+    {#if data.user}
+      <form method="POST" action="?/logout" use:enhance>
+        <button
+          type="submit"
+          class="flex items-center justify-center gap-2 text-md w-32 h-12 bg-red-500 hover:bg-red-800 text-black px-4 py-2 rounded-lg transition-colors"
+        >
+          Logout
+          <LogOut size={16} />
+        </button>
+      </form>
+    {:else}
+      <a
+        href="/login"
+        class="flex items-center justify-center gap-2 text-md w-32 h-12 bg-white hover:bg-gray-300 text-black px-4 py-2 rounded-lg transition-colors"
+      >
+        Login
+        <LogIn size={16} />
+      </a>
+    {/if}
   </div>
 </nav>
 
---
+++ src/routes/(app)/+page.server.ts
@@ -0,0 +1,19 @@
+import { deleteSession, deleteSessionCookie } from "$lib/server/session";
+import type { Actions } from "@sveltejs/kit";
+import { redirect } from "@sveltejs/kit";
+export const actions: Actions = {
+  logout: async (event) => {
+    if (!event.locals.user) {
+      throw new Error("User not found");
+    }
+
+    const sessionId = event.cookies.get("session");
+    
+    if (sessionId) {
+      await deleteSession(sessionId);
+      deleteSessionCookie(event);
+    }
+
+    redirect(302, "/");
+  },
+};
